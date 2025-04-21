e8d841105e0d13813f422fa7d9acc205b0f623ef
reireizo <nathan.irwin2021@gmail.com>
2025-04-21 11:56:14 -0400

    ATB Bar Moves

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -5,4 +5,56 @@ using UnityEngine;
 public class EnemyStateMachine : MonoBehaviour
 {
     public BaseEnemy enemy;
+
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+    float currentCooldown = 0f;
+    float maxCooldown = 7f;
+
+    void Start()
+    {
+        currentState = TurnState.PROCESSING;
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case TurnState.PROCESSING:
+                UpdateProgressBar();
+                break;
+            case TurnState.ADDTOLIST:
+
+                break;
+            case TurnState.WAITING:
+
+                break;
+            case TurnState.SELECTING:
+
+                break;
+            case TurnState.ACTION:
+
+                break;
+            case TurnState.DEAD:
+
+                break;
+        }
+    }
+
+    void UpdateProgressBar()
+    {
+        currentCooldown += Time.deltaTime;
+        if (currentCooldown >= maxCooldown)
+        {
+            currentState = TurnState.ADDTOLIST;
+        }
+    }
 }
--- Assets/Scripts/PlayerStateMachine.cs
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -1,8 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStateMachine : MonoBehaviour
 {
     public BasePlayer player;
+
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+    float currentCooldown = 0f;
+    float maxCooldown = 5f;
+    public Image ProgressBar;
+
+    void Start()
+    {
+        currentState = TurnState.PROCESSING;
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case TurnState.PROCESSING:
+                UpdateProgressBar();
+                break;
+            case TurnState.ADDTOLIST:
+
+                break;
+            case TurnState.WAITING:
+
+                break;
+            case TurnState.SELECTING:
+
+                break;
+            case TurnState.ACTION:
+
+                break;
+            case TurnState.DEAD:
+
+                break;
+        }
+    }
+
+    void UpdateProgressBar()
+    {
+        currentCooldown += Time.deltaTime;
+        float percentCooldown = currentCooldown / maxCooldown;
+        ProgressBar.transform.localScale = new Vector3(Mathf.Clamp(percentCooldown, 0, 1), ProgressBar.transform.localScale.y, ProgressBar.transform.localScale.z);
+        if (currentCooldown >= maxCooldown)
+        {
+            currentState = TurnState.ADDTOLIST;
+        }
+    }
 }
